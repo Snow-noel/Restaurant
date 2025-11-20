58e4545f380caf11a8d7f07fbea40ce1fd590650
Snow <bis23-smsumba@mubas.ac.mw>
2025-11-20 16:41:39 +0200

    added the home page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import { home } from "./home";
 //import "../src/pictures/restaurant.jpg";
 import "./styles.css";
-const button=document.getElementById("home")
-button.addEventListener("click",()=>{
+const buttonhome=document.getElementById("home")
+buttonhome.addEventListener("click",()=>{
+    const main =document.getElementById("main-content");
+    main.innerHTML="";
+    home();
+})
+const buttonmenu=document.getElementById("menu")
+buttonmenu.addEventListener("click",()=>{
     const main =document.getElementById("main-content");
     main.innerHTML="";
     home();
---
+++ src/menu.js
@@ -0,0 +1,36 @@
+ import barger from "./pictures/homeimg.jpeg"
+
+export const home=()=>{
+   const main =document.getElementById("main-content");
+   const section=document.createElement("section");
+      const content= document.createElement('div');
+      const message=document.createElement('h3')
+      const top=document.createElement('h2')
+       const bottom=document.createElement('h2')
+      const myimage=document.createElement('img');
+      top.classList.add("top");
+      bottom.classList.add("bottom");
+      section.classList.add("shows");
+      bottom.textContent="Feel Free With Snow Our Chef"
+      top.textContent="Welcome To Kumadya"
+      myimage.src=barger;
+      myimage.style.cssText=`
+      height:50%;
+      width:100%;
+      border-radius:10px;
+      `
+      content.classList.add("content")
+      myimage.classList.add('home-image');
+      message.classList.add('welcome')
+      message.textContent=`
+         Where every meal is prepared with passion, flavor, and a touch of home.
+         Whether you're here for a quick bite or a full feast, 
+         we promise fresh tastes, warm smiles, and unforgettable moments.`;
+      main.append(section);
+      section.append(myimage,content);
+      content.append(top,message,bottom)
+      setTimeout(()=>{
+         section.classList.add("show")
+      },15)
+
+}
